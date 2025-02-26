5d754af1cba1270ee30d766d1e2d42db79f44be8
swarna123467 <afrinaswarna085@gmail.com>
2025-02-26 12:20:12 +0600

    my login form created

---
+++ js/login.js
@@ -0,0 +1,8 @@
+// console.log('button clicking file added')
+
+document.getElementById('button-login').addEventListener('click', function (event) {
+    event.preventDefault();
+    console.log('login button clicked')
+    const phoneNumber = document.getElementById('phone-number').value;
+    console.log(phoneNumber)
+})
