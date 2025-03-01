566531b74d5634c00c3fff89f35ca295b440998d
swarna123467 <afrinaswarna085@gmail.com>
2025-03-01 11:25:25 +0600

    home page added

--- js/login.js
+++ js/login.js
@@ -1,8 +1,31 @@
 // console.log('button clicking file added')
 
+// document.getElementById('button-login').addEventListener('click', function (event) {
+//     event.preventDefault();
+//     console.log('login button clicked')
+//     const phoneNumber = document.getElementById('phone-number').value;
+//     const pinNumber = document.getElementById('pin-number').value;
+//     console.log(phoneNumber, pinNumber)
+
+//     if (phoneNumber === '5' && pinNumber === '1234') {
+//         console.log('You are logged in')
+//         window.location.href = "/home.html";
+//     }
+//     else {
+//         alert('wrong phone number or pin number')
+//     }
+// })
 document.getElementById('button-login').addEventListener('click', function (event) {
     event.preventDefault();
-    console.log('login button clicked')
     const phoneNumber = document.getElementById('phone-number').value;
-    console.log(phoneNumber)
+    const pinNumber = document.getElementById('pin-number').value;
+    // console.log(phoneNumber, pinNumber);
+    if (phoneNumber === '5' && pinNumber === '1234') {
+        console.log('you are logged in');
+        window.location.href = '/home.html';
+    }
+    else {
+        alert('wrong phone number or pin');
+    }
+
 })
