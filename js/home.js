aa7f5de7bdf1cfcf8f89249eae380ce23e159fdf
swarna123467 <afrinaswarna085@gmail.com>
2025-03-03 21:43:58 +0600

    added amount and pin number

--- js/home.js
+++ js/home.js
@@ -1,1 +1,9 @@
 console.log('home added')
+
+document.getElementById('btn-add-money').addEventListener('click', function (event) {
+    event.preventDefault();
+    const addMoneyAmount = document.getElementById('add-money-amount').value;
+    console.log(addMoneyAmount)
+    const pinNumber = document.getElementById('pin-number-added').value;
+    console.log(pinNumber)
+})
