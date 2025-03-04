40725906af5482ddbf4c1877037d07972220071f
swarna123467 <afrinaswarna085@gmail.com>
2025-03-04 11:37:43 +0600

    complete add money section

--- js/home.js
+++ js/home.js
@@ -6,4 +6,18 @@ document.getElementById('btn-add-money').addEventListener('click', function (eve
     console.log(addMoneyAmount)
     const pinNumber = document.getElementById('pin-number-added').value;
     console.log(pinNumber)
+
+    if (pinNumber === '1234') {
+        console.log('add money into your balance')
+        const availableBalance = document.getElementById('balance').innerText;
+        console.log(typeof availableBalance)
+        const addMoneyNumber = parseFloat(addMoneyAmount);
+        const balance = parseFloat(availableBalance);
+        const newBalance = addMoneyNumber + balance;
+        console.log(newBalance);
+        document.getElementById('balance').innerText = newBalance;
+    }
+    else {
+        alert('wrong pin number.please try again!')
+    }
 })
